813878dd6e31cb39ec8d41815557d1e62752d900
fillipesreis <96076999+fillipesreis@users.noreply.github.com>
2026-09-25 09:01:51 -0300

    Initial commit of the API Produtos project

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { produtoRoutes } from "./routes/produtoRoutes.js";
+
+export const app = express();
+
+//Middleware: "ensina" o express a ler json no corpo da requisição
+app.use(express.json());
+
+app.get('/api/check', (req, res) => {
+    res.status(200).json({ status: 'ok', mensagem: 'Servidor funcionando via HTTP!' });
+});
+
+app.use('/api/produto', produtoRoutes);
+
+app.use((req, res) => {
+    res.status(404).json({ erro: `A rota ${req.method} ${req.originalUrl} não existe.` });
+});
+
+app.use((erro, req, res, _next) => {
+    console.error('Erro de Sistema: erro.message');
+    res.status(500).json({ erro: 'Falha interna do servidor' });
+});
